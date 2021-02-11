d2ee52247b2cd1d492d9859691de44724e2e3a9e
Pablo Bejarano <pabhoz@gmail.com>
2021-02-11 08:58:00 -0500

    Adición de luz y sombras

--- class1/src/app.js
+++ class1/src/app.js
@@ -6,51 +6,99 @@ let speed = 0.01;
 
 function main() {
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
     document.body.appendChild(renderer.domElement);
 
-    // cube = drawCube(0xff00ff, true);
-    // scene.add(cube);
 
+    // Lights
+    setUpLights();
+
+    /*
     const geometry = new THREE.BufferGeometry();
     const vertices = new Float32Array( [
         0.0, 1.0,  1.0,
         -1.0, 0.0,  1.0,
         1.0,  0.0,  1.0
-    ] );
-
+    ]);
+    
+    // https://threejs.org/docs/index.html#api/en/core/BufferGeometry.normalizeNormals
+    // geometry.setIndex([]);
     geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-
+    
     cube = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
         color: 0xff0000,
-        wireframe: true,
+        wireframe: false,
     }));
 
-    scene.add(cube);
+    
+
+    // scene.add(cube);
+    */
+
+    el = drawCube(0xFF0000);
+    el.position.z = 0;
+    el.position.y = 3;
+    scene.add(el);
+
+    plane = drawPlane(10, 10, 4, 4, 0x404040, true);
+    plane.rotation.x = Math.PI / 2;
+    scene.add(plane);
+
 
     camera.position.z = 5;
-    // camera.position.y = 5;
-    // camera.lookAt(cube.position);
+    camera.position.y = 5;
+    camera.lookAt(el.position);
 
     animate();
 }
 
 function drawCube(color, wireframe = false) {
     const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({
+    const material = new THREE.MeshPhongMaterial({
         color,
         wireframe
     });
-    return new THREE.Mesh(geometry, material);
+    const cube = new THREE.Mesh(geometry, material);
+    cube.castShadow = true;
+    return cube;
+}
+
+function drawPlane(w, h, sw, sh, color, ds = false) {
+    const geometry = new THREE.PlaneGeometry( w,h, sw, sh );
+    const material = new THREE.MeshPhongMaterial({ color, side: ds ? THREE.DoubleSide : undefined });
+    const plane = new THREE.Mesh(geometry, material);
+    plane.receiveShadow = true;
+    return plane;
 }
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    /*cube.rotation.x += speed;
-    cube.rotation.y += speed;*/
+    el.rotation.x += speed;
+    el.rotation.y += speed;
+
+    // cube.rotation.x += speed;
 
     const speedLabel = document.querySelector("#speed");
     speedLabel.innerHTML = speed;
+}
+
+function setUpLights() {
+    const ambient = new THREE.AmbientLight( 0xffffff, 0.1 );
+    scene.add( ambient );
+
+    spotLight = new THREE.SpotLight( 0xffffff, 1 );
+    spotLight.position.set( 0, 10, 0 );
+    spotLight.angle = Math.PI / 4;
+    spotLight.penumbra = 0.1;
+    spotLight.decay = 2;
+    spotLight.distance = 200;
+
+    spotLight.castShadow = true;
+    scene.add( spotLight );
+
+     // lightHelper = new THREE.SpotLightHelper( spotLight );
+     // scene.add( lightHelper );
 }
 
 function speedUp() {
